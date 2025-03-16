5695ec7393d2e6a0c59b2ecc7eea43efae52f0bb
kaany <74436329+Kaanydev@users.noreply.github.com>
2025-03-16 16:48:29 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+class Kaany:
+  def __init__(self, blusa, calca, tenis):
+    self.blusa = blusa
+    self.calca = calca
+    self.tenis = tenis
+
+kaany = kaany("Verde", "Preta", "Nike")
+print(f"Kaany está vestido com uma blusa{kaany.blusa}, uma calça {kaany.calca}, e um tenis da marca {kaany.tenis}")
