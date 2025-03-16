b7ec0884662709b5f24e22e8e7765e9d55478b98
kaany <74436329+Kaanydev@users.noreply.github.com>
2025-03-16 16:50:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ class Kaany:
     self.calca = calca
     self.tenis = tenis
 
-kaany = kaany("Verde", "Preta", "Nike")
+kaany = Kaany("Verde", "Preta", "Nike")
 print(f"Kaany está vestido com uma blusa{kaany.blusa}, uma calça {kaany.calca}, e um tenis da marca {kaany.tenis}")
